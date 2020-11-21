eb58e8fc6b68804e3ee34accfc250bff170fd55d
leo <algo2000@naver.com>
2020-11-22 01:53:38 +0900

    update ani test

--- ani_2020-11-21_01/js/app.js
+++ ani_2020-11-21_01/js/app.js
@@ -1,113 +1,30 @@
-var stageWidth;
-var stageHeight;
-var maxTop;
-var minTop;
-var gap = 0;
-
-var movePosY;
-var startPosY;
-var startTopPosY;
-
-var isClick = false;
-
-var barAni;
-
-$( document ).ready( function(){
-    $( window ).resize( function(){
-        initSet();
-    });
-    initSet();
+$(document).on('touchstart','body',function(e)
+{
 });
 
-function initSet()
-{   
-    stageWidth = (Number)($( window ).width());
-    stageHeight = (Number)($( window ).height());        
-    maxTop = stageHeight*0.07;
-    minTop = stageHeight*0.93;
-    $("#contents").animate({
-        "top" : minTop + "px"
-    },100);
-}
+var isTouch = false;
+var mousePos;
 
-$('#drag-bar').on('touchstart',function(e) 
+$(document).on('touchstart','#contents',function(e)
 {
-    startPosY = e.originalEvent.touches[0].clientY;
-    startTopPosY = (Number)($("#contents").css("top").replace("px",""));
-    movePosY = e.originalEvent.touches[0].clientY;
-    gap = movePosY - startPosY;
-    isClick = true;
+    isTouch = true;
 });
-
-$('#drag-bar').on('touchmove',function(e) 
+$(document).on('touchmove','#contents',function(e)
 {
-    if(isClick)
+    if(isTouch)
     {
-        movePosY = e.originalEvent.touches[0].clientY;
-        gap = movePosY - startPosY;
-        if($('#content').scrollTop() === 0)
-        {
-            if(maxTop <= startTopPosY + gap && minTop >= startTopPosY + gap)
-            {
-                $("#contents").css({
-                    "top" : startTopPosY + gap +"px"
-                });
-            }
-        }
-    }
-});
-
-$('#drag-bar').on('touchend',function(e) 
-{
-    isClick = false;
-    var top = (Number)($("#contents").css("top").replace("px",""))
-    var ratioM;
-    var ratioP;
-    if(gap>=0)
-    {
-        ratioP = 0.2;
-        ratioM = 1 - ratioP;
-    }
-    else
-    {
-        ratioM = 0.2;
-        ratioP = 1 - ratioM;
-    }
-
-    if(maxTop <= top && top <= stageHeight*ratioP)
-    {
-        setAnimationAuto(-1);
-    }
-    else
-    {        
-        setAnimationAuto(1);
-    }
-});
-
-$('#content').on('touchend touchstart touchmove',function(e) 
-{
-    if($('#content').scrollTop() <= 0)
-    {
-        e.stopPropagation();
-        alert("dafsd");
-    }
-});
-
-function setAnimationAuto(vec)
-{
-    if(vec < 0)
-    {
-        $("#contents").animate({
-            "top" : maxTop + "px"
-        },100);
-    }
-    else
-    {
-        $("#contents").animate({
-            "top" : minTop + "px"
-        }, 100, function()
-        {
-            $('#content').scrollTop(0);
+        mousePos = e.originalEvent.touches[0].clientY;
+        $('#contents').css({
+            'top' : mousePos + "px"
         });
     }
-}
+});
+$(document).on('touchend','#contents',function(e)
+{
+    isTouch = false;
+});
+
+$(document).on('touchstart touchmove touchend scroll','#content',function(e)
+{
+    e.stopPropagation();
+});
