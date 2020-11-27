b0a4b9bb9aa11771a157b6fc4288f586363e7aa4
leo <algo2000@naver.com>
2020-11-27 14:39:39 +0900

    no message

--- ani_2020-11-26_03/js/test.js
+++ ani_2020-11-26_03/js/test.js
@@ -1,20 +1,31 @@
 let json = require("./tags.json");
-console.log(json);
+// console.log(json);
 let result = [];
+var count = 0;
+let text = "pos";
+const regex = new RegExp('^[0-9a-zA-Z]([0-9a-zA-Z])*:'+text, 'gi');
+// for(let i = 0; i<json.length;i++)
+// {
+//     if(json[i].search(regex)!=-1)
+//     {
+//         result.push(json[i]);
+//         if(result.length == 5)
+//         {
+//             break;
+//         }
+//     }
+// }
 
-let text = "anal";
-const regex = new RegExp(text, 'gi');
-for(let i = 0; i<json.length;i++)
-{
-    if(json[i].search(regex)!=-1)
+json.some(data =>{
+    if(data.search(regex)!=-1)
     {
-        result.push(json[i]);
+        result.push(data);
         if(result.length == 5)
         {
-            break;
+            return true;
         }
     }
-}
+});
 
 result.forEach(element => {
     console.log(element);
